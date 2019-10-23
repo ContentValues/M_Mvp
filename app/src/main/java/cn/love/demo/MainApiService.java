ad271485732629d039591be7e1389f2749fd3c8c
zhangmin1 <736126375@qq.com>
2019-10-23 10:55:50 +0800

    udpate mainapiservice

--- app/src/main/java/cn/love/demo/MainApiService.java
+++ app/src/main/java/cn/love/demo/MainApiService.java
@@ -1,20 +1,38 @@
 package cn.love.demo;
 
 
+import cn.love.demo.model.GankResults;
 import cn.love.model.http.BaseApiService;
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * @author：LiuXiaoDong on 2018/4/20 18:26.
  */
 
-public interface MainApiService extends BaseApiService {
+public interface MainApiService /*extends BaseApiService*/ {
 
     @FormUrlEncoded
     @POST("userRegister/login")
     Observable<ResponseBody> login(@Field("mobile") String mobile, @Field("code") String code);
+
+
+//    @GET("data/{type}/{number}/{page}")
+//    Flowable<GankResults> getGankData(@Path("type") String type,
+//                                      @Path("number") int pageSize,
+//                                      @Path("page") int pageNum);
+
+
+    @GET("data/{type}/{number}/{page}")
+    Observable<GankResults> getGankData(@Path("type") String type,
+                                        @Path("number") int pageSize,
+                                        @Path("page") int pageNum);
+
+
 }
